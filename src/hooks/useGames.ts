4b89511bfa36249353053df52aff34988bc5dec9
Damilare <bolarinwaoluwadamilare91@gmail.com>
2024-02-05 23:52:22 +0100

    Sort the games

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,6 +1,5 @@
 import { GameQuery } from "../App";
 import useData from "./useData";
-import { Genre } from "./useGenres";
 
 export interface platform{
   id:number;
@@ -18,7 +17,9 @@ export interface platform{
 
 const useGames = (gameQuery: GameQuery) => useData<Game>('/games', 
 {params:{
-  genres: gameQuery.genre?.id, platforms:gameQuery.platform?.id
+  genres: gameQuery.genre?.id, 
+  platforms:gameQuery.platform?.id,
+  ordering: gameQuery.sortOrder
 }}, 
 [gameQuery]);
 
